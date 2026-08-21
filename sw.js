b3b577b19ad2897427c0325f6c2502d623e1eb19
wlghks24 <7cnpk87b76@privaterelay.appleid.com>
2026-08-21 20:48:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tcg-v31-country-photo-box-15';
+const CACHE='tcg-v31-unified-precision-16';
 const CORE=['./','./index.html','./manifest.webmanifest','./releases.json','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
