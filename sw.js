100972df7c79e84393316d5da92383cb543b99f5
wlghks24 <7cnpk87b76@privaterelay.appleid.com>
2026-08-22 23:51:59 +0900

    fix: refresh cached site after Korean Pokémon event update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tcg-v31-pc-pages-unified-35';
+const CACHE='tcg-v31-pokemon-kr-events-36';
 const CORE=['./','./index.html','./manifest.webmanifest','./releases.json','./promo_events.json','./purchase_sources.json','./market_prices.json','./market_watch.json','./exchange_rates.json','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
