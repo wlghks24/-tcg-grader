ed9e5f563a7323e792879c738c245657967180f0
wlghks24 <7cnpk87b76@privaterelay.appleid.com>
2026-09-04 02:16:16 +0900

    chore: remove temporary v6 source patch wrapper

--- apply_source_gap_v6_sources.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import apply_source_gap_v6 as patch
-
-patch.patch_provider_health()
-patch.patch_routes()
-patch.patch_multichannel()
-patch.patch_social()
-patch.patch_pipeline()
-print("v6 source-only patch applied")
