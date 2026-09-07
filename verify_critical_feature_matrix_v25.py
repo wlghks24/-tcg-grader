ab29b1c02ba2b7620129d727cce5edf24f78ec6c
wlghks24 <7cnpk87b76@privaterelay.appleid.com>
2026-09-07 10:23:30 +0900

    CODEMAP fast router: sync verify_critical_feature_matrix_v25.py

--- verify_critical_feature_matrix_v25.py
+++ verify_critical_feature_matrix_v25.py
@@ -30,6 +30,9 @@ FEATURE_FILES = {
         "test_ocr_multistage_v16.py",
     ],
     "ocr_extended_verification": [
+        "library_slab_corpus.py",
+        "manual_graded_photo_registration.py",
+        "graded_photo_evidence.py",
         "ocr_accuracy_boost_v147.py",
         "ocr_front_back_fallback_v148.py",
         "grading_cert_verifier.py",
@@ -123,6 +126,8 @@ FEATURE_FILES = {
     "code_map_internal": [
         "verify_code_map_internal.py",
         "code_map_intelligence.py",
+        "code_map_fast_route.py",
+        "test_code_map_fast_route_v195.py",
         "GRAPHIFY_UPDATE.sh",
         "GRAPHIFY_SELF_HEAL.py",
         "GRAPHIFY_AUDIT.py",
